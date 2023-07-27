29bbfe9b7d2b0f4e63982df52a1bb55c63e5b6f6
Leavaway <505871488@qq.com>
2023-07-27 23:22:37 +0800

    complete timer lab

---
+++ hardware/timer.h
@@ -0,0 +1,7 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+void Timer_Init(void);
+uint16_t Timer_GetCounter(void);
+
+#endif
--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
-#include "CountSensor.h"
+#include "timer.h"
 
 int16_t num;
 
 int main(void) {
 	OLED_Init();
-	CountSensor_Init();
+	Timer_Init();
 	
-	OLED_ShowString(1,1,"Num");
+	OLED_ShowString(1,1,"Num:");
+	OLED_ShowString(2,1,"Cnt:");
+	//OLED_Clear();
 	
     while(1){
-		num += Get_Count();
-		OLED_ShowSignedNum(1,5,num,5);
+		OLED_ShowNum(1,5,num,5);
+		OLED_ShowNum(2,5,Timer_GetCounter(),5);
 	}; // Add infinite loop at the end to keep the microcontroller running
 
     return 0;
