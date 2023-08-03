021d6e2aca7da8e5ec62f9a0e4328ff3aebcddde
Leavaway <505871488@qq.com>
2023-08-03 20:57:55 +0800

    complete adc and dma lab

--- main.c
+++ main.c
@@ -5,30 +5,15 @@
 #include "encoder.h"
 #include "ad.h"
 
-int16_t i,j,k,l;
-float v;
-uint8_t KeyNum;
-int8_t speed;
-
 
 int main(void) {
 	OLED_Init();
 	AD_Init();
-	OLED_ShowString(1,1,"AD0;");
-	OLED_ShowString(2,1,"AD1;");
-	OLED_ShowString(3,1,"AD2;");
-	OLED_ShowString(4,1,"AD3;");
+
 	
     while(1){
-		i = AD_GetValue(ADC_Channel_0);
-		j = AD_GetValue(ADC_Channel_1);
-		k = AD_GetValue(ADC_Channel_2);
-		l = AD_GetValue(ADC_Channel_3);
-		OLED_ShowNum(1,5,i,4);
-		OLED_ShowNum(2,5,j,4);
-		OLED_ShowNum(3,5,k,4);
-		OLED_ShowNum(4,5,l,4);
-		
+		OLED_ShowNum(1,5,AD_val[0],4);
+		OLED_ShowNum(1,5,AD_val[1],4);
 		Delay_ms(100);
 	}; // Add infinite loop at the end to keep the microcontroller running
 
@@ -43,5 +28,7 @@ void TIM2_IRQHandler(void){
 	}
 }
 */
+
+
 
 
---
+++ system/mydma.h
@@ -0,0 +1,7 @@
+#ifndef _MYDMA_H
+#define _MYDMA_H
+
+void Mydma_Init(uint32_t AddrA, uint32_t AddrB,uint32_t DMA_BufferSize);
+void Mydma_Transfer(void);
+
+#endif
